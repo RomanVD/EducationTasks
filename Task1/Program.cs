4bd4681b24c3f9138bfd485365ce5c658041b40a
Roman Dolgopolov <romanvd255@mail.ru>
2017-12-11 10:53:50 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationTasks.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Почему статический конструктор не может иметь параметров?
+
+            //Какие действия выполняет оператор new?
+
+            //В каком порядке инициализируются свойства и поля?
+
+            //Чему будет равно SpecialMoney, DefaultMoney и PublicKey?
+
+            //Чему равно this в конструкторе экземпляра и в статическом конструкторе?
+            
+            var child = new MyChildClass("text", 1);
+
+            //* Изменится ли порядок инициализации статических свойст DefaultMoney и SpecialMoney, если раскомментировать строку 59?            
+
+            //* Нарисуй примерную схему того, что будет находиться в оперативной памяти после вызова new MyChildClass("text", 1)
+
+            //* Можно ли объявить такое поле public const MyChildClass = new MyChildClass(); в каком нибудь другом классе?
+        }
+    }
+
+    internal class MyParentClass
+    {
+        protected static decimal DefaultMoney { get; set; } = 5m;
+
+        public const long Identificator = 123456789012345;
+        static MyParentClass()
+        {
+            DefaultMoney = 10m;
+        }
+
+        protected MyParentClass(int number)
+        {
+            Number = number;
+        }
+
+        public int Number { get; set; }
+    }
+
+    internal class MyChildClass : MyParentClass
+    {
+        internal readonly double PublicKey = 99.9999999d;
+
+        public static readonly decimal SpecialMoney = 20m;
+
+        static MyChildClass()
+        {
+            //DefaultMoney = 15m;
+            SpecialMoney = 30m;            
+        }
+
+        public MyChildClass(string text, int number) : base(number)
+        {
+            PublicKey = 100.1111111d;
+            Text = text;
+        }
+
+        public string Text { get; set; }
+    }
+
+    class test
+    {
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+//Что находится в каждой из этих сборок?
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace EducationTasks.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Проиллюстрируй в коде иерархию наследования коллекций.
+        }
+    }
+
+    class MyIEnumerable : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class MyIEnumerable<T> : MyIEnumerable, IEnumerable<T>
+    {
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class MyICollection<T> : MyIEnumerable<T>, ICollection<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Замени строку (35) "class MyICollection<T> : MyIEnumerable<T>, ICollection<T>" на "class MyICollection<T> : ICollection<T>", посмотри что получится.
+    //Продолжай, их не так уж много. Гугл в помощь.
+    //Что бы не терять читаемость, переноси классы в отдельные файлы или используй регионы (#region   #endregion)
+}
